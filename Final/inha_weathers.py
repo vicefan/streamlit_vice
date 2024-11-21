dfd62c587c4843715cdbe374bfd6fdca3dd6f8a7
vicefan <dddooong2000@naver.com>
2024-11-21 21:16:39 +0900

    UPDATE

--- Final/inha_weathers.py
+++ Final/inha_weathers.py
@@ -5,7 +5,8 @@ from folium.features import CustomIcon
 from streamlit_folium import st_folium
 import streamlit as st
 
-st.set_page_config(page_title="INHA_WEATHERS", page_icon="🫠", layout="wide")
+st.set_page_config(page_title="INHA_WEATHERS", page_icon="🫠", layout="wide",
+                   menu_items={"Report a bug": "dddooong2000@naver.com"})
 st.subheader("🫠 인하대학교 주변 날씨 🫠")
 st.write("🌡️ 온도는 ℃로 표시됩니다.")
 st.write("👉🏼 이모지를 눌러 날씨 정보를 확인하세요.")
