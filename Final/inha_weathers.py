b798b4961a9213b58eb14bb78f8db89a6b08409f
vicefan <dddooong2000@naver.com>
2024-11-23 02:27:31 +0900

    Update

--- Final/inha_weathers.py
+++ Final/inha_weathers.py
@@ -1,30 +1,9 @@
-# This Python file uses the following encoding: utf-8
-import urllib.request, json, folium
-import geopandas as gpd
-from folium.features import CustomIcon
+import json, folium, urllib.request
 from streamlit_folium import st_folium
 import streamlit as st
+from folium.features import CustomIcon
 
-st.set_page_config(page_title="INHA_WEATHERS", page_icon="🫠", layout="wide",
-                   menu_items={"About": "www.instagram.com/rollingloud/viceversartist"})
-st.header("🫠인하대학교 주변 날씨🫠")
-st.write("🌡️온도는 ℃로 표시됩니다.")
-st.write("👉🏼이모지를 눌러 날씨 정보를 확인하세요.")
-c_box = st.checkbox("✅날씨 정보 보기")
-
-if c_box:
-    locations = {
-        "dohwa": ["Final/Geo_Split/dohwa.geojson", (37.469248, 126.660751)],
-        "ganseok": ["Final/Geo_Split/ganseok.geojson", (37.461129, 126.703504)],
-        "guwol": ["Final/Geo_Split/guwol.geojson", (37.447230, 126.706705)],
-        "gwankyo": ["Final/Geo_Split/gwankyo.geojson", (37.440732, 126.686613)],
-        "hakik": ["Final/Geo_Split/hakik.geojson", (37.434989, 126.649610)],
-        "juan": ["Final/Geo_Split/juan.geojson", (37.457082, 126.677223)],
-        "sunge": ["Final/Geo_Split/sunge.geojson", (37.461215, 126.645630)],
-        "yonghyun": ["Final/Geo_Split/yonghyun.geojson", (37.451925, 126.647227)],
-        }
-
-    weathers = {
+weathers = {
         "2": ["뇌우", "#00008b", "⛈"],
         "3": ["이슬비", "#add8e6", "☔"],
         "5": ["비", "#0000ff", "☔"],
@@ -38,149 +17,83 @@ if c_box:
     }
 
 
-    def get_weather(lat, lon):
-        f = urllib.request.urlopen(f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&APPID=c3114ccaa3eada02fe5d90aefc5f249c")
-        s = json.loads(f.read())
-        return s
-
-
-    def style(location):
-        weather_id = str(weather_info[location]['weather'][0]['id'])
-        if weather_id[0] == "7" or weather_id[0] == "8":
-            loc_style = {
-                'fillColor': weathers[weather_id][1],
-                'fillOpacity': 0.7,
-                'color': 'black',
-                'weight': 1,
-                "dashArray": "5, 5"
-            }
-        else:
-            loc_style = {
-                'fillColor': weathers[weather_id[0]][1],
-                'fillOpacity': 0.7,
-                'color': 'black',
-                'weight': 1,
-                "dashArray": "5, 5"
-            }
-
-        return lambda x: loc_style
-
-    def get_icon(loc):
-        anchor = {
-            "dohwa": (10, 50),
-            "ganseok": (10, 50),
-            "guwol": (10, 50),
-            "sunge": (10, 50),
-            "yonghyun": (30, 45),
-            "juan": (10, 20),
-            "gwankyo": (-30, 30),
-            "hakik": (0, 80),
+def style(location):
+    weather_id = str(get_weather(location[0], location[1])['weather'][0]['id'])
+    if weather_id[0] == "7" or weather_id[0] == "8":
+        loc_style = {
+            'fillColor': weathers[weather_id][1],
+            'fillOpacity': 0.7,
+            'color': 'black',
+            'weight': 1,
+            "dashArray": "5, 5"
+        }
+    else:
+        loc_style = {
+            'fillColor': weathers[weather_id[0]][1],
+            'fillOpacity': 0.7,
+            'color': 'black',
+            'weight': 1,
+            "dashArray": "5, 5"
         }
 
-        icon = CustomIcon(
-            icon_image=f"https://openweathermap.org/img/wn/{weather_info[loc]['weather'][0]['icon']}@2x.png",
-            icon_size=(70, 70) if loc != "gwankyo" else (40, 40),
-            icon_anchor=anchor[loc],
-        )
-
-        return icon
-
-    def gpd_naming(x):
-        return gpd.read_file(locations[x][0])
+    return lambda x: loc_style
 
 
-    dohwa = gpd_naming("dohwa")
-    ganseok = gpd_naming("ganseok")
-    guwol = gpd_naming("guwol")
-    gwankyo = gpd_naming("gwankyo")
-    hakik = gpd_naming("hakik")
-    juan = gpd_naming("juan")
-    sunge = gpd_naming("sunge")
-    yonghyun = gpd_naming("yonghyun")
+def calc_center(loc):
+    global coordinates
+    geo_average = [0, 0]
+    for i in data['features']:
+        if i['properties']['SIG_KOR_NM'] == loc:
+            coordinates = i
+            co_len = len(i['geometry']['coordinates'][0])
+            for co in i['geometry']['coordinates'][0]:
+                geo_average[0] += co[0]
+                geo_average[1] += co[1]
 
-    weather_info = {name: get_weather(lat, lon) for name, (path, (lat, lon)) in locations.items()}
+    geo_average[0] /= co_len
+    geo_average[1] /= co_len
 
-    emoji_dohwa, color_dohwa = weathers[str(weather_info["dohwa"]['weather'][0]['id'])][2], weathers[str(weather_info["dohwa"]['weather'][0]['id'])][1]
-    emoji_ganseok, color_ganseok = weathers[str(weather_info["ganseok"]['weather'][0]['id'])][2], weathers[str(weather_info["ganseok"]['weather'][0]['id'])][1]
-    emoji_guwol, color_guwol = weathers[str(weather_info["guwol"]['weather'][0]['id'])][2], weathers[str(weather_info["guwol"]['weather'][0]['id'])][1]
-    emoji_gwankyo, color_gwankyo = weathers[str(weather_info["gwankyo"]['weather'][0]['id'])][2], weathers[str(weather_info["gwankyo"]['weather'][0]['id'])][1]
-    emoji_hakik, color_hakik = weathers[str(weather_info["hakik"]['weather'][0]['id'])][2], weathers[str(weather_info["hakik"]['weather'][0]['id'])][1]
-    emoji_juan, color_juan = weathers[str(weather_info["juan"]['weather'][0]['id'])][2], weathers[str(weather_info["juan"]['weather'][0]['id'])][1]
-    emoji_sunge, color_sunge = weathers[str(weather_info["sunge"]['weather'][0]['id'])][2], weathers[str(weather_info["sunge"]['weather'][0]['id'])][1]
-    emoji_yonghyun, color_yonghyun = weathers[str(weather_info["yonghyun"]['weather'][0]['id'])][2], weathers[str(weather_info["yonghyun"]['weather'][0]['id'])][1]
+    return geo_average[::-1]
 
-    popup_dohwa = folium.Popup(
+
+def get_weather(lat, lon):
+    f = urllib.request.urlopen(f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&APPID=c3114ccaa3eada02fe5d90aefc5f249c")
+    s = json.loads(f.read())
+    return s
+
+
+with open('Final/Geo_All/TL_SCCO_SIG.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+geo_kr_names = [i['properties']['SIG_KOR_NM'] for i in data['features']]
+
+st.set_page_config(page_title="Test_Weather", page_icon="🫠",
+                   menu_items={"About": "www.instagram.com/rollingloud/viceversartist"})
+st.header("🫠전국 날씨 탐색기🫠")
+select_loc = st.selectbox(label=":pushpin:지역을 선택하세요!",options=geo_kr_names, index=None, placeholder="Choose a Location!")
+
+if select_loc is not None:
+    coo_cen = calc_center(select_loc)
+
+    weather_info = get_weather(coo_cen[0], coo_cen[1])
+    weathers_id = str(get_weather(coo_cen[0], coo_cen[1])['weather'][0]['id'])
+
+    mapa = folium.Map(location=coo_cen, zoom_start=10, tiles='cartodbpositron')
+    loc = folium.GeoJson(coordinates, style_function=style(coo_cen))
+    loc.add_to(mapa)
+
+    popup = folium.Popup(
         f'''
-        <h1 style="color:{color_dohwa}"><strong>도화동</strong> 날씨</h1><h1>{emoji_dohwa}</h1>
-        <strong>온도</strong> : {weather_info["dohwa"]["main"]["temp"]} °C<br>
-        <strong>체감온도</strong> : {weather_info["dohwa"]["main"]["feels_like"]} °C
+        <h1 style="color:{weathers[weathers_id][1]}"><strong>{select_loc}</strong> 날씨</h1><h1>{weathers[weathers_id][2]}</h1>
+        🌡<strong>온도</strong> : {weather_info["main"]["temp"]} °C<br>
+        🌡<strong>체감온도</strong> : {weather_info["main"]["feels_like"]} °C
     ''', max_width=400)
 
-    popup_ganseok = folium.Popup(
-        f'''
-        <h1 style="color:{color_ganseok}"><strong>간석동</strong> 날씨</h1><h1>{emoji_ganseok}</h1>
-        <strong>온도</strong> : {weather_info["ganseok"]["main"]["temp"]} °C<br>
-        <strong>체감온도</strong> : {weather_info["ganseok"]["main"]["feels_like"]} °C
-    ''', max_width=400)
+    marker = folium.Marker(location=coo_cen, popup=popup, icon=CustomIcon(
+        icon_image="https://cdn-icons-png.flaticon.com/512/4585/4585518.png",
+        icon_size=(70, 70)
+    ))
+    marker.add_to(mapa)
+    st_folium(mapa, width=600, height=450)
 
-    popup_guwol = folium.Popup(
-        f'''
-        <h1 style="color:{color_guwol}"><strong>구월동</strong> 날씨</h1><h1>{emoji_guwol}</h1>
-        <strong>온도</strong> : {weather_info["guwol"]["main"]["temp"]} °C<br>
-        <strong>체감온도</strong> : {weather_info["guwol"]["main"]["feels_like"]} °C
-    ''', max_width=400)
 
-    popup_gwankyo = folium.Popup(
-        f'''
-        <h1 style="color:{color_gwankyo}"><strong>관교동</strong> 날씨</h1><h1>{emoji_gwankyo}</h1>
-        <strong>온도</strong> : {weather_info["gwankyo"]["main"]["temp"]} °C<br>
-        <strong>체감온도</strong> : {weather_info["gwankyo"]["main"]["feels_like"]} °C
-    ''', max_width=400)
-
-    popup_hakik = folium.Popup(
-        f'''
-        <h1 style="color:{color_hakik}"><strong>학익동</strong> 날씨</h1><h1>{emoji_hakik}</h1>
-        <strong>온도</strong> : {weather_info["hakik"]["main"]["temp"]} °C<br>
-        <strong>체감온도</strong> : {weather_info["hakik"]["main"]["feels_like"]} °C
-    ''', max_width=400)
-
-    popup_juan = folium.Popup(
-        f'''
-        <h1 style="color:{color_juan}"><strong>주안동</strong> 날씨</h1><h1>{emoji_juan}</h1>
-        <strong>온도</strong> : {weather_info["juan"]["main"]["temp"]} °C<br>
-        <strong>체감온도</strong> : {weather_info["juan"]["main"]["feels_like"]} °C
-    ''', max_width=400)
-
-    popup_sunge = folium.Popup(
-        f'''
-        <h1 style="color:{color_sunge}"><strong>숭의동</strong> 날씨</h1><h1>{emoji_sunge}</h1>
-        <strong>온도</strong> : {weather_info["sunge"]["main"]["temp"]} °C<br>
-        <strong>체감온도</strong> : {weather_info["sunge"]["main"]["feels_like"]} °C
-    ''', max_width=400)
-
-    popup_yonghyun = folium.Popup(
-        f'''
-        <h1 style="color:{color_yonghyun}"><strong>용현동</strong> 날씨</h1><h1>{emoji_yonghyun}</h1>
-        <strong>온도</strong> : {weather_info["yonghyun"]["main"]["temp"]} °C<br>
-        <strong>체감온도</strong> : {weather_info["yonghyun"]["main"]["feels_like"]} °C
-    ''', max_width=400)
-
-    mapa = folium.Map(location=(37.45, 126.68), zoom_start=13, tiles='Cartodb Positron')
-    g_dohwa = folium.GeoJson(dohwa, name="도화", style_function=style("dohwa")).add_to(mapa)
-    folium.Marker(location=locations["dohwa"][1], popup=popup_dohwa, icon=get_icon("dohwa")).add_to(mapa)
-    g_ganseok = folium.GeoJson(ganseok, name="간석", style_function=style("ganseok")).add_to(mapa)
-    folium.Marker(location=locations["ganseok"][1], popup=popup_ganseok, icon=get_icon("ganseok")).add_to(mapa)
-    g_guwol = folium.GeoJson(guwol, name="구월", style_function=style("guwol")).add_to(mapa)
-    folium.Marker(location=locations["guwol"][1], popup=popup_guwol, icon=get_icon("guwol")).add_to(mapa)
-    g_gwankyo = folium.GeoJson(gwankyo, name="관교", style_function=style("gwankyo")).add_to(mapa)
-    folium.Marker(location=locations["gwankyo"][1], popup=popup_gwankyo, icon=get_icon("gwankyo")).add_to(mapa)
-    g_hakik = folium.GeoJson(hakik, name="학익", style_function=style("hakik")).add_to(mapa)
-    folium.Marker(location=locations["hakik"][1], popup=popup_hakik, icon=get_icon("hakik")).add_to(mapa)
-    g_juan = folium.GeoJson(juan, name="주안", style_function=style("juan")).add_to(mapa)
-    folium.Marker(location=locations["juan"][1], popup=popup_juan, icon=get_icon("juan")).add_to(mapa)
-    g_sunge = folium.GeoJson(sunge, name="숭의", style_function=style("sunge")).add_to(mapa)
-    folium.Marker(location=locations["sunge"][1], popup=popup_sunge, icon=get_icon("sunge")).add_to(mapa)
-    g_yonghyun = folium.GeoJson(yonghyun, name="용현", style_function=style("yonghyun")).add_to(mapa)
-    folium.Marker(location=locations["yonghyun"][1], popup=popup_yonghyun, icon=get_icon("yonghyun")).add_to(mapa)
-
-    st_folium(mapa, width=1400, height=600, center=(37.45, 126.68), zoom=13)
