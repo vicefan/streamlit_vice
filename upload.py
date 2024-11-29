0023ad96f9223180b6199ecef53261aefd251a55
vicefan <dddooong2000@naver.com>
2024-11-30 07:57:57 +0900

    UPDATE

--- upload.py
+++ upload.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 from PyPDF2 import PdfReader
 
 # OpenAI API key 설정
-api_key = st.secrets["apikey"]
+api_key = st.secrets["api_key"]
 client = OpenAI(api_key=api_key)
 
 st.set_page_config(layout="wide")
