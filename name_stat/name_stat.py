8a02212e7985669514b776fd6c81bf6009b765e6
vicefan <dddooong2000@naver.com>
2025-04-03 22:03:46 +0900

    Update

--- name_stat/name_stat.py
+++ name_stat/name_stat.py
@@ -1,22 +1,15 @@
 import base64
 import streamlit as st
 
-fam_name, given_name = input("성 : "), input("이름 : ")
-
-raw_str = f"{fam_name}{given_name},1,{given_name},{fam_name},Y"
-
-encoded_str = base64.b64encode(raw_str.encode()).decode()
-
-link = f"https://www.credit.co.kr/ib20/mnu/BZWMNLGNM20?param={encoded_str}&uaCheck=Y"
-print(link)
-
-
 st.set_page_config(layout="wide")
 
 # Streamlit 애플리케이션
-st.title("ChatGPT PDF Application")
+st.title("나이스지키미 로그인 시키지마라")
 cols = st.columns(2)
 with cols[0]:
     st.subheader("이름을 적어주세요.")
     fam_name = st.text_input("성")
     given_name = st.text_input("이름")
+    raw_str = f"{fam_name}{given_name},1,{given_name},{fam_name},Y"
+    encoded_str = base64.b64encode(raw_str.encode()).decode()
+    link = f"https://www.credit.co.kr/ib20/mnu/BZWMNLGNM20?param={encoded_str}&uaCheck=Y"
