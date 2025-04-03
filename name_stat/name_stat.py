33a1a842ac1164a432d1c48304c895e1b290b960
vicefan <dddooong2000@naver.com>
2025-04-04 00:43:45 +0900

    Update

--- name_stat/name_stat.py
+++ name_stat/name_stat.py
@@ -41,6 +41,8 @@ def get_screenshot(app_url):
 
     # Explicitly wait for an essential element to ensure content is loaded
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+    driver.save_screenshot('screenshot.png')
+    st.image('./screenshot.png')
 
     myid = driver.find_element(By.XPATH, '//*[@id="userId"]')
     myid.send_keys("dddooong2000")
@@ -48,6 +50,8 @@ def get_screenshot(app_url):
     mypw.send_keys("Chan0thug!")
     mybtn = driver.find_element(By.XPATH, '//*[@id="idLoginBtn"]')
     mybtn.click()
+    driver.save_screenshot('screenshot.png')
+    st.image('./screenshot.png')
 
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
 
